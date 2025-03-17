d3e67366900474bd3f3cd35d799a747ebe622352
Weverton11 <weverton2001a@gmail.com>
2025-03-17 17:24:19 -0300

    inclusão de if e else

--- scripts.js
+++ scripts.js
@@ -4,9 +4,14 @@ function generateNumber() {
 
     const max = Math.floor(document.querySelector(".input-max").value)
 
-    const result = Math.floor(Math.random() * (max - min + 1)) + min;
+    if (min >= max) {
+        alert("O valor mínimo deve ser MENOR que o valor máximo")
+    } else {
+        const result = Math.floor(Math.random() * (max - min + 1)) + min;
 
-    alert(result)
+        alert(result)
+    }
+
 
 }
 
